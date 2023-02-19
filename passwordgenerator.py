ea5626d0bfa7829a53a1857a90774d2d7321102d
Lucas Magno <lucasamagno@gmail.com>
2023-02-18 19:53:49 -0500

    some code

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -2,6 +2,30 @@ import random
 import string
 
 def generate_password(min_length, numbers=True, special_characters=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special_chars = string.punctuation
+
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters+= special_chars
+
+    pwd = ""
+    meets_criteria = False
+    has_number = False
+    has_special = False
+
+    while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in digits:
+            has_number = True
+        elif new_char in special_chars:
+            has_special = True
+
     pass
 
 generate_password(10)
